4b66eee56b943e7a07eb30b87b62396351b71914
mikan <83708535+piny940@users.noreply.github.com>
2022-05-26 14:35:27 +0900

    Create QuickSort

--- assignment8/src/main/java/QuickSort.java
+++ assignment8/src/main/java/QuickSort.java
@@ -2,12 +2,36 @@ import java.util.ArrayList;
 
 public class QuickSort {
     public static void sort(ArrayList<Integer> list) {
-        
+        partSort(list, 0, list.size());
     }
 
-    public static void swap(ArrayList<Integer> list, Integer i, Integer j) {
-        Integer x = list.get(i);
-        list.set(i, list.get(j));
-        list.set(j, x);
+    public static void partSort(ArrayList<Integer> list, int startIdx, int endIdx) {
+        if (endIdx - startIdx <= 1) {
+            return;
+        }
+
+        int pivot = getPivot(list, startIdx, endIdx);
+        Integer pivotValue = list.get(pivot);
+        Integer pivotValueNum = 1; // The number of values equal to pivot value.
+        for (int i = 0; i < endIdx - startIdx; i++) {
+            if (list.get(i) < pivotValue) {
+                Integer value = list.get(i);
+                list.remove(i);
+                list.add(pivot, value);
+                pivot++;
+            }
+            else if (list.get(i) == pivotValue) {
+                Integer value = list.get(i);
+                list.remove(i);
+                list.add(pivot, value);
+                pivotValueNum++;
+            }
+        }
+        partSort(list, startIdx, pivot);
+        partSort(list, pivot+pivotValueNum, endIdx);
+    }
+
+    public static int getPivot(ArrayList<Integer> list, int startIdx, int endIdx) {
+        return startIdx;
     }
 }
