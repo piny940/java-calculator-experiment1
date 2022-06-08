574b600af12668f904b5e4c6510f028774c8e7e3
mikan <83708535+piny940@users.noreply.github.com>
2022-06-08 15:09:33 +0900

    Copy MyGraph class from assignment10

---
+++ assignment12/src/main/java/MyGraphTest.java
@@ -0,0 +1,38 @@
+import java.util.InputMismatchException;
+import java.util.LinkedList;
+import java.util.NoSuchElementException;
+import java.util.Scanner;
+
+public class MyGraphTest {
+    public static void main(String[] args) {
+        try {
+            Scanner scanner = new Scanner(System.in);
+            String filename = scanner.nextLine();
+            int id = Integer.parseInt(scanner.nextLine());
+            scanner.close();
+
+            MyGraph myGraph = new MyGraph();
+            myGraph.readFromFile(filename);
+            LinkedList<MyEdge> linkedEdges = myGraph.getEdges(id);
+
+            for (MyEdge edge : linkedEdges) {
+                Integer[] nodes = edge.getNodes();
+                Integer node = nodes[0] != id ? nodes[0] : nodes[1];
+                Integer weight = edge.getWeight();
+
+                System.out.println(String.format("%d %d", node, weight));
+            }
+        }
+        catch (InputMismatchException ex) {
+            System.out.println(ex.getMessage());
+            System.exit(0);
+        }
+        catch (NoSuchElementException ex) {
+            System.out.println("The actual number of edges given is less than the number of edges specified in the file.");
+        }
+        catch (RuntimeException ex) {
+            System.out.println(ex.getMessage());
+            System.exit(0);
+        }
+    }
+}
