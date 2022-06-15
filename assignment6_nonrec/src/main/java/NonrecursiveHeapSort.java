cd79b4012b3142f5228de1a954de474f74645831
mikan <83708535+piny940@users.noreply.github.com>
2022-06-15 15:06:03 +0900

    NonrecursiveHeapSort: Reduce calculation time

--- assignment6_nonrec/src/main/java/NonrecursiveHeapSort.java
+++ assignment6_nonrec/src/main/java/NonrecursiveHeapSort.java
@@ -6,12 +6,13 @@ public class NonrecursiveHeapSort {
         list.clear();
 
         buildHeap(heapList);
+        int size = heapList.size();
 
         for (int i = heapList.size()-1; i >= 0; i--) {
             list.add(heapList.get(0));
             heapList.set(0, heapList.get(i));
-            heapList.remove(i);
-            heapify(heapList, 0);
+            size--;
+            heapify(heapList, 0, size);
         }
     }
 
@@ -21,26 +22,26 @@ public class NonrecursiveHeapSort {
         list.set(j, x);
     }
 
-    public static void heapify(ArrayList<Integer> list, Integer rootIdx) {
+    public static void heapify(ArrayList<Integer> list, Integer rootIdx, int size) {
         Integer pIdx = rootIdx;
 
-        while (getMinIdx(list, pIdx) != pIdx) {
-            int minIdx = getMinIdx(list, pIdx);
+        while (getMinIdx(list, pIdx, size) != pIdx) {
+            int minIdx = getMinIdx(list, pIdx, size);
             swap(list, pIdx, minIdx);
             pIdx = minIdx;
         }
     }
 
-    public static int getMinIdx(ArrayList<Integer> list, Integer pIdx) {
+    public static int getMinIdx(ArrayList<Integer> list, Integer pIdx, int size) {
         int[] childrenIdx = getChildrenIdx(pIdx);
         int lcIdx = childrenIdx[0]; // The index of the left child.
         int rcIdx = childrenIdx[1]; // The index of the right child.
         int minIdx = pIdx;
 
-        if (lcIdx <= list.size()-1 && list.get(lcIdx) < list.get(minIdx)) {
+        if (lcIdx <= size-1 && list.get(lcIdx) < list.get(minIdx)) {
             minIdx = lcIdx;
         }
-        if (rcIdx <= list.size()-1 && list.get(rcIdx) < list.get(minIdx)) {
+        if (rcIdx <= size-1 && list.get(rcIdx) < list.get(minIdx)) {
             minIdx = rcIdx;
         }
         return minIdx;
@@ -58,7 +59,7 @@ public class NonrecursiveHeapSort {
 
     public static void buildHeap(ArrayList<Integer> list) {
         for (int i = list.size()-1; i >= 0; i--) {
-            heapify(list, i);
+            heapify(list, i, list.size());
         }
     }
 }
--- assignment6_nonrec/src/test/java/NonrecursiveHeapSortTest.java
+++ assignment6_nonrec/src/test/java/NonrecursiveHeapSortTest.java
@@ -12,7 +12,7 @@ public class NonrecursiveHeapSortTest {
         ArrayList<Integer> list = new ArrayList<>(
             Arrays.asList(1, 8, 4, 6, 5, 6, 7, 8, 7, 6, 8, 7, 8, 9, 8)
         );
-        NonrecursiveHeapSort.heapify(list, 1);
+        NonrecursiveHeapSort.heapify(list, 1, list.size());
         ArrayList<Integer> expected = new ArrayList<>(
             Arrays.asList(1, 5, 4, 6, 6, 6, 7, 8, 7, 8, 8, 7, 8, 9, 8)
         );
@@ -23,7 +23,7 @@ public class NonrecursiveHeapSortTest {
     public void testHeapify2() {
         ArrayList<Integer> list2 = new ArrayList<>(
                 Arrays.asList(3, 2, 4));
-        NonrecursiveHeapSort.heapify(list2, 0);
+        NonrecursiveHeapSort.heapify(list2, 0, list2.size());
         ArrayList<Integer> expected2 = new ArrayList<>(
                 Arrays.asList(2, 3, 4));
         assertEquals(expected2, list2);
